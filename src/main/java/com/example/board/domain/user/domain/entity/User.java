18eea3f9e87ff2652c2d6e1ba705b76541759540
김대웅 <dkssud9556@dsm.hs.kr>
2020-10-30 02:41:17 +0900

    유저 엔티티

---
+++ src/main/java/com/example/board/domain/user/domain/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.board.domain.user.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @Column(length = 20)
+    private String username;
+
+    @Column(length = 30)
+    private String password;
+
+    @Column(length = 10)
+    private String name;
+
+    @Column
+    private int age;
+}
