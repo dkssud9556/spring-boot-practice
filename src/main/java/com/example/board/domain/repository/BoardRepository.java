c77eeb469bd93568146e1711efc57a8e994c1a87
김대웅 <dkssud9556@dsm.hs.kr>
2020-10-29 22:41:41 +0900

    [ADD] hibernate validator and something

---
+++ src/main/java/com/example/board/domain/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.example.board.domain.repository;
+
+import com.example.board.domain.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+}
