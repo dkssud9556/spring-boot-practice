b2ea39aed8eb1093202bcfc020133ab784001de9
김대웅 <dkssud9556@dsm.hs.kr>
2020-10-31 01:36:51 +0900

    sign in dto

--- src/main/java/com/example/board/domain/user/dto/SignInRequest.java
+++ src/main/java/com/example/board/domain/user/dto/SignInRequest.java
@@ -1,4 +1,19 @@
 package com.example.board.domain.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignInRequest {
+
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String password;
 }
---
+++ src/main/java/com/example/board/domain/user/dto/SignInResponse.java
@@ -0,0 +1,12 @@
+package com.example.board.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignInResponse {
+    private String token;
+}
