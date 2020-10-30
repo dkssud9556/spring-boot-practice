11a4cc4520e63db1a663ad51939849a6b711e8bd
김대웅 <dkssud9556@dsm.hs.kr>
2020-10-31 01:34:52 +0900

    [ADD] sign in controller

--- src/main/java/com/example/board/domain/user/controller/UserController.java
+++ src/main/java/com/example/board/domain/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.board.domain.user.controller;
 
+import com.example.board.domain.user.dto.SignInRequest;
+import com.example.board.domain.user.dto.SignInResponse;
 import com.example.board.domain.user.dto.SignUpRequest;
 import com.example.board.domain.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +20,10 @@ public class UserController {
     @PostMapping
     public void signUp(@RequestBody SignUpRequest signUpRequest) {
         userService.signUp(signUpRequest);
+    }
+
+    @PostMapping("/signin")
+    public SignInResponse signIn(@RequestBody SignInRequest signInRequest) {
+        return userService.signIn(signInRequest);
     }
 }
