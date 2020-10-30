6e9acc03f55c4d1054baf19a66a63a46c9bb7db5
김대웅 <dkssud9556@dsm.hs.kr>
2020-10-31 01:37:27 +0900

    sign in 서비스 추가

--- src/main/java/com/example/board/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/example/board/domain/user/service/UserServiceImpl.java
@@ -1,11 +1,18 @@
 package com.example.board.domain.user.service;
 
+import com.example.board.domain.user.domain.TokenProvider;
 import com.example.board.domain.user.domain.entity.User;
 import com.example.board.domain.user.domain.repository.UserRepository;
+import com.example.board.domain.user.dto.SignInRequest;
+import com.example.board.domain.user.dto.SignInResponse;
 import com.example.board.domain.user.dto.SignUpRequest;
+import com.example.board.domain.user.exeptions.InvalidLoginInfoException;
+import com.example.board.domain.user.exeptions.UserAlreadyExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -13,14 +20,18 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private TokenProvider tokenProvider;
+
     @Override
     @Transactional
     public void signUp(SignUpRequest signUpRequest) {
-        User user = userRepository.findById(signUpRequest.getUsername());
-        if (user != null) {
-            throw new IllegalArgumentException("유저네임 중복");
-            // TODO 유저네임 중복 예외
+        Optional<User> user = userRepository.findById(signUpRequest.getUsername());
+
+        if (user.isPresent()) {
+            throw new UserAlreadyExistsException();
         }
+
         userRepository.save(
                 User.builder()
                 .username(signUpRequest.getUsername())
@@ -29,5 +40,18 @@ public class UserServiceImpl implements UserService {
                 .name(signUpRequest.getName())
                 .build()
         );
+    }
+
+    @Override
+    public SignInResponse signIn(SignInRequest signInRequest) {
+        User user = userRepository.findById(signInRequest.getUsername())
+                .orElseThrow(InvalidLoginInfoException::new); // 로그인 실패
+
+        if (!user.isCorrectPassword(signInRequest.getPassword())) {
+            throw new InvalidLoginInfoException();
+        }
+
+        String token = tokenProvider.generateToken(signInRequest.getUsername());
+        return new SignInResponse(token);
     }
 }
