9ac59243f4bfb2fbbc4875700f72f3a73a21c845
김대웅 <dkssud9556@dsm.hs.kr>
2020-11-03 23:35:52 +0900

    게시판 컨트롤러 추가

---
+++ src/main/java/com/example/board/domain/board/controller/BoardController.java
@@ -0,0 +1,9 @@
+package com.example.board.domain.board.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/boards")
+@RestController
+public class BoardController {
+}
