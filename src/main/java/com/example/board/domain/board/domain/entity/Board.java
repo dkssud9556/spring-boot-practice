cb4b414557cf48dfcde14c3bce069027f0d683f3
김대웅 <dkssud9556@dsm.hs.kr>
2020-11-05 19:43:59 +0900

    board 엔티티

---
+++ src/main/java/com/example/board/domain/board/domain/entity/Board.java
@@ -0,0 +1,25 @@
+package com.example.board.domain.board.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "board_id")
+    private Long boardId;
+
+    @Column(length = 50)
+    private String title;
+
+    @Column(length = 200)
+    private String content;
+}
