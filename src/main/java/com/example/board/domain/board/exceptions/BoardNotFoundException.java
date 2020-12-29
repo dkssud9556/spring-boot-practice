10a223aa3fad90fc96dd2238e2a684af8ef8ec7a
김대웅 <dkssud9556@dsm.hs.kr>
2020-12-29 16:26:41 +0900

    [ADD] board not found exception

---
+++ src/main/java/com/example/board/domain/board/exceptions/BoardNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.board.domain.board.exceptions;
+
+import com.example.board.global.error.exception.BusinessException;
+import com.example.board.global.error.exception.ErrorCode;
+
+public class BoardNotFoundException extends BusinessException {
+    public BoardNotFoundException() {
+        super(ErrorCode.BOARD_NOT_FOUND);
+    }
+}
--- src/main/java/com/example/board/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/board/global/error/exception/ErrorCode.java
@@ -8,12 +8,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    BOARD_NOT_FOUND(404, "Board is not Found"),
+
     BAD_REQUEST(400, "Bad Request(Invalid Parameter)"),
     USER_DUPLICATION(409, "User is Already Exists"),
     INVALID_LOGIN_INFO(400, "Login Information is Invalid"),
 
-    USER_NOT_FOUND(404, "User is not found"),
-    INVALID_TOKEN(401, "Invalid token");
+    USER_NOT_FOUND(404, "User is not Found"),
+    INVALID_TOKEN(401, "Invalid Token");
 
 
     private final int status;
