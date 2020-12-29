9898025590505f83a85a125877b5def07942c316
김대웅 <dkssud9556@dsm.hs.kr>
2020-12-29 16:27:38 +0900

    [ADD] findByBoardId method

--- src/main/java/com/example/board/domain/board/domain/repository/BoardRepository.java
+++ src/main/java/com/example/board/domain/board/domain/repository/BoardRepository.java
@@ -3,8 +3,10 @@ package com.example.board.domain.board.domain.repository;
 import com.example.board.domain.board.domain.entity.Board;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BoardRepository {
     List<Board> findAll();
     Board save(Board board);
+    Optional<Board> findByBoardId(Long boardId);
 }
--- src/main/java/com/example/board/domain/board/infra/JpaBoardRepository.java
+++ src/main/java/com/example/board/domain/board/infra/JpaBoardRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface JpaBoardRepository extends JpaRepository<Board, Long>, BoardRepository {
@@ -14,4 +15,7 @@ public interface JpaBoardRepository extends JpaRepository<Board, Long>, BoardRep
 
     @Override
     Board save(Board board);
+
+    @Override
+    Optional<Board> findByBoardId(Long boardId);
 }
