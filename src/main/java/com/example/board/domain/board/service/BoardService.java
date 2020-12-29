aa12463bccf5cdb223cb8027ba1599331413e959
김대웅 <dkssud9556@dsm.hs.kr>
2020-12-29 16:27:20 +0900

    [ADD] modify board method

--- src/main/java/com/example/board/domain/board/service/BoardService.java
+++ src/main/java/com/example/board/domain/board/service/BoardService.java
@@ -1,7 +1,13 @@
 package com.example.board.domain.board.service;
 
+import com.example.board.domain.board.domain.entity.Board;
 import com.example.board.domain.board.dto.PostBoardRequest;
+import com.example.board.domain.board.dto.PutBoardRequest;
+
+import java.util.List;
 
 public interface BoardService {
     void postBoard(PostBoardRequest postBoardRequest);
+    List<Board> getAll();
+    void modifyBoard(Long boardId, PutBoardRequest putBoardRequest);
 }
--- src/main/java/com/example/board/domain/board/service/BoardServiceImpl.java
+++ src/main/java/com/example/board/domain/board/service/BoardServiceImpl.java
@@ -3,9 +3,13 @@ package com.example.board.domain.board.service;
 import com.example.board.domain.board.domain.entity.Board;
 import com.example.board.domain.board.domain.repository.BoardRepository;
 import com.example.board.domain.board.dto.PostBoardRequest;
+import com.example.board.domain.board.dto.PutBoardRequest;
+import com.example.board.domain.board.exceptions.BoardNotFoundException;
 import com.example.board.global.security.AuthenticationFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -16,10 +20,24 @@ public class BoardServiceImpl implements BoardService {
 
     @Override
     public void postBoard(PostBoardRequest postBoardRequest) {
-        boardRepository.save(Board.builder()
-                .content(postBoardRequest.getContent())
-                .title(postBoardRequest.getTitle())
-                .username(authenticationFacade.getUsername())
-                .build());
+        boardRepository.save(
+                postBoardRequest.toEntityWithUsername(authenticationFacade.getUsername())
+        );
+    }
+
+    @Override
+    public List<Board> getAll() {
+        return boardRepository.findAll();
+    }
+
+    @Override
+    public void modifyBoard(Long boardId, PutBoardRequest putBoardRequest) {
+        Board board = boardRepository.findByBoardId(boardId)
+                .orElseThrow(BoardNotFoundException::new);
+
+        board.setTitle(putBoardRequest.getTitle());
+        board.setContent(putBoardRequest.getContent());
+
+        boardRepository.save(board);
     }
 }
