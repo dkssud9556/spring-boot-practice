96a89ecf10a0d761af750dbeecae93055f15e879
김대웅 <dkssud9556@dsm.hs.kr>
2020-12-29 16:25:37 +0900

    [ADD] toEntity method in PostBoardRequest

--- src/main/java/com/example/board/domain/board/dto/PostBoardRequest.java
+++ src/main/java/com/example/board/domain/board/dto/PostBoardRequest.java
@@ -1,5 +1,6 @@
 package com.example.board.domain.board.dto;
 
+import com.example.board.domain.board.domain.entity.Board;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,4 +11,12 @@ import lombok.NoArgsConstructor;
 public class PostBoardRequest {
     private String title;
     private String content;
+
+    public Board toEntityWithUsername(String username) {
+        return Board.builder()
+                .username(username)
+                .title(title)
+                .content(content)
+                .build();
+    }
 }
